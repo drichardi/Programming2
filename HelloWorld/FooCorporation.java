ef8bd6c7f94aa2f38f1ae398e994f7ea67fbfcb4
Dan Richardi <d_richardi@sau9.org>
2016-12-06 00:18:48 -0500

    added JavaDoc

--- HelloWorld/FooCorporation.java
+++ HelloWorld/FooCorporation.java
@@ -1,10 +1,23 @@
+/**
+ * FooCorporation class for computing weekly pay for employees.
+ */
+
 public class FooCorporation {
     
-    final static double MINIMUM_WAGE = 8.0;
+    /** Default $8.00 per hour */
+    final static double MINIMUM_WAGE = 8.0; 
+    /** Total of 40 hours at base pay rate before overtime rate */
     final static double REGULAR_PAY_HOURS = 40.0;
-    final static double MAX_TOTAL_HOURS = 60.0;
-    final static double OVERTIME_RATE_ADJUST = 1.5;
+    /**FooCorporation does not allow for working more than 60 hours */
+    final static double MAX_TOTAL_HOURS = 60.0; 
+    /** Overtime pay is adjusted 1.5 x base rate */
+    final static double OVERTIME_RATE_ADJUST = 1.5; 
     
+    /**
+     * Calculate Employee Pay:
+     * basePay must be equal to or greater than current MINIMUM_WAGE
+     * Overtime wage calculated per hour over 40 hours at adjusted rate
+     */
     public static void Pay(double basePay, double hoursWorked) {
         if( basePay < MINIMUM_WAGE ) {
             System.out.printf( "Error. Base pay must be equal to or above $%.2f.\n", MINIMUM_WAGE);
@@ -22,10 +35,14 @@ public class FooCorporation {
         }
     }
     public static void main(String[] args) {
+        /**
+         * Test Cases:
+         */
         Pay(7.50, 35);
         Pay(9.00, 38.5);
         Pay(8.20, 47);
         Pay(10.00, 73);
+        
         
     }
 }
